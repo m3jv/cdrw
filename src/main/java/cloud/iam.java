c891fce9678ec16ba07be1ac3b0f08042dfe848c
Boyan ILIEV <boyan_iliev@carrefour.com>
2021-08-17 11:01:03 +0200

    init tests

---
+++ src/main/java/cloud/iam.java
@@ -0,0 +1,36 @@
+package aws.example.iam;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
+import com.amazonaws.services.identitymanagement.model.CreateUserRequest;
+import com.amazonaws.services.identitymanagement.model.CreateUserResult;
+
+/**
+ * Creates an IAM user
+ */
+public class CreateUser {
+
+    public static void main(String[] args) {
+
+        final String USAGE =
+            "To run this example, supply a username\n" +
+            "Ex: CreateUser <username>\n";
+
+        if (args.length != 1) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
+        String username = args[0];
+
+        final AmazonIdentityManagement iam =
+            AmazonIdentityManagementClientBuilder.defaultClient();
+
+        CreateUserRequest request = new CreateUserRequest()
+            .withUserName(username);
+
+        CreateUserResult response = iam.createUser(request);
+
+        System.out.println("Successfully created user: " +
+                response.getUser().getUserName());
+    }
+}
