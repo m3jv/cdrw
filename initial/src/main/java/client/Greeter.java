350ecdd0511eea6f139bafd9a42d05a8af94f56f
m3jv <89033458+m3jv@users.noreply.github.com>
2022-03-24 15:27:20 +0100

    Update Greeter.java

--- initial/src/main/java/client/Greeter.java
+++ initial/src/main/java/client/Greeter.java
@@ -2,6 +2,6 @@ package client;
 
 public class Greeter {
 	public String sayHello() {
-		return "Hello Client - Jenkins is greate product!";
+		return "Hello Client - Jenkins is great product !!!";
 	}
 }
